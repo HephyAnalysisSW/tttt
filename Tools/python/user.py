1a0b0b64f2e94efd06db65c87dedc776b7d056f2
cristina.giordano <cristina.giordano@oeaw.ac.at>
2022-10-10 16:05:32 +0200

    adding cutInterpreter.py

--- Tools/python/user.py
+++ Tools/python/user.py
@@ -7,3 +7,11 @@ if os.environ["USER"] in ["dennis.schwarz"]:
     cache_dir                       = "/groups/hephy/cms/dennis.schwarz/tWZ/caches"
     # Analysis result files
     cern_proxy_certificate          = "/users/dennis.schwarz/.private/.proxy"
+if os.environ["USER"] in ["cristina.giordano"]:
+    postprocessing_output_directory = "/scratch-cbe/users/cristina.giordano/tttt/nanoTuples"
+    postprocessing_tmp_directory    = "/scratch/hephy/cms/cristina.giordano/tttt/tmp/"
+    plot_directory                  = "/groups/hephy/cms/cristina.giordano/www/tttt/plots"
+    cache_dir                       = "/groups/hephy/cms/cristina.giordano/tttt/caches"
+    # Analysis result files
+    cern_proxy_certificate          = "/users/cristina.giordano/.private/.proxy"
+
