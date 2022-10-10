1b89a30e341426a012563aa5d82e5acaeb383e69
Maryam Shooshtari <mary.shooshtari@gmail.com>
2022-10-10 16:48:45 +0200

    adding Maryam to user

--- Tools/python/user.py
+++ Tools/python/user.py
@@ -1,9 +1,9 @@
 import os
 
-if os.environ["USER"] in ["dennis.schwarz"]:
-    postprocessing_output_directory = "/scratch-cbe/users/dennis.schwarz/tttt/nanoTuples"
-    postprocessing_tmp_directory    = "/scratch/hephy/cms/dennis.schwarz/tttt/tmp/"
-    plot_directory                  = "/groups/hephy/cms/dennis.schwarz/www/tttt/plots"
-    cache_dir                       = "/groups/hephy/cms/dennis.schwarz/tWZ/caches"
+if os.environ["USER"] in ["maryam.shooshtari"]:
+    postprocessing_output_directory = "/scratch-cbe/users/maryam.shooshtari/tttt/nanoTuples"
+    postprocessing_tmp_directory    = "/scratch/hephy/cms/maryam.shooshtari/tttt/tmp/"
+    plot_directory                  = "/groups/hephy/cms/maryam.shooshtari/www/tttt/plots"
+    cache_dir                       = "/groups/hephy/cms/maryam.shooshtari/tttt/caches"
     # Analysis result files
-    cern_proxy_certificate          = "/users/dennis.schwarz/.private/.proxy"
+    cern_proxy_certificate          = "/users/maryam.shooshtari/.private/.proxy"
