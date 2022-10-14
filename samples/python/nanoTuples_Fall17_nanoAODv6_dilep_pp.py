9553241ea734d4378813d12e84107e7a3cbee94f
Robert Schoefbeck <robert.schoefbeck@cern.ch>
2022-10-14 09:38:58 +0200

    adding private UL20 production

---
+++ samples/python/nanoTuples_Fall17_nanoAODv6_dilep_pp.py
@@ -0,0 +1,68 @@
+
+#/scratch-cbe/users/robert.schoefbeck/tWZ/nanoTuples/tWZ_nAODv6_private_v3/2016/dilep:
+#DYJetsToLL_M10to50             DYJetsToLL_M50_HT100to200_ext   DYJetsToLL_M50_HT400to600_comb  DYJetsToLL_M50_LO_ext1_comb          DYJetsToLL_M5to50_HT400to600_comb  TTTT
+#DYJetsToLL_M10to50_LO          DYJetsToLL_M50_HT1200to2500     DYJetsToLL_M50_HT600to800       DYJetsToLL_M50_LO_ext1_comb_lheHT70  DYJetsToLL_M5to50_HT600toInf       TTWW
+#DYJetsToLL_M10to50_LO_lheHT70  DYJetsToLL_M50_HT200to400_comb  DYJetsToLL_M50_HT70to100        DYJetsToLL_M5to50_HT100to200_comb    DYJetsToLL_M5to50_HT70to100        TTWZ
+#DYJetsToLL_M50_ext2            DYJetsToLL_M50_HT2500toInf      DYJetsToLL_M50_HT800to1200      DYJetsToLL_M5to50_HT200to400_comb    TTLep_pow                          TTZZ
+#
+#/scratch-cbe/users/robert.schoefbeck/tWZ/nanoTuples/tWZ_nAODv6_private_v3/2017/dilep:
+#DYJetsToLL_M10to50_LO           DYJetsToLL_M4to50_HT200to400_comb  DYJetsToLL_M50_HT100to200       DYJetsToLL_M50_HT2500toInf  DYJetsToLL_M50_HT70to100    DYJetsToLL_M50_LO_comb_lheHT70  TTWW
+#DYJetsToLL_M10to50_LO_lheHT100  DYJetsToLL_M4to50_HT400to600_comb  DYJetsToLL_M50_HT1200to2500     DYJetsToLL_M50_HT400to600   DYJetsToLL_M50_HT800to1200  TTLep_pow                       TTWZ
+#DYJetsToLL_M4to50_HT100to200    DYJetsToLL_M4to50_HT600toInf_comb  DYJetsToLL_M50_HT200to400_comb  DYJetsToLL_M50_HT600to800   DYJetsToLL_M50_LO_comb      TTTT                            TTZZ
+
+
+
+#DYJetsToLL_M10to50_LO
+#DYJetsToLL_M50_HT1200to2500
+#DYJetsToLL_M50_HT2500toInf
+#DYJetsToLL_M50_HT600to800
+#DYJetsToLL_M50_HT800to1200
+#DYJetsToLL_M50_HT100to200
+#DYJetsToLL_M50_HT200to400
+#DYJetsToLL_M50_HT400to600
+#DYJetsToLL_M50_HT70to100
+#DYJetsToLL_M50_LO_lheHT70
+
+import os
+from RootTools.core.Sample import Sample
+import ROOT
+
+# Logging
+import logging
+logger = logging.getLogger(__name__)
+
+from tttt.samples.color import color
+
+directory_ = "/scratch-cbe/users/robert.schoefbeck/tWZ/nanoTuples/tWZ_nAODv6_private_v4/2017/dilep"
+
+def make_dirs( dirs ):
+    return [ os.path.join( directory_, dir_ ) for dir_ in dirs ]
+
+dirs = {}
+
+dirs['TTLep']  = ["TTLep_pow"]
+TTLep = Sample.fromDirectory(name="TTLep", treeName="Events", isData=False, color=color.TT, texName="t#bar{t}", directory=make_dirs( dirs['TTLep'] ))
+
+dirs['DYJetsToLL']  = ["DYJetsToLL_M50_LO_comb"]
+DYJetsToLL = Sample.fromDirectory(name="DYJetsToLL", treeName="Events", isData=False, color=color.DY, texName="DYJetsToLL", directory=make_dirs( dirs['DYJetsToLL'] ))
+
+dirs['TTH']  = ["TTHbb", "TTHnobb_pow"]
+TTH = Sample.fromDirectory(name="TTH", treeName="Events", isData=False, color=color.TTH, texName="t#bar{t}H", directory=make_dirs( dirs['TTH'] ))
+
+dirs['TTW']  = ["TTWToLNu", "TTWToQQ"]
+TTW = Sample.fromDirectory(name="TTW", treeName="Events", isData=False, color=color.TTW, texName="t#bar{t}W", directory=make_dirs( dirs['TTW'] ))
+
+dirs['TTZ']  = ["TTZToLLNuNu", "TTZToLLNuNu_m1to10", "TTZToQQ"]
+TTZ = Sample.fromDirectory(name="TTZ", treeName="Events", isData=False, color=color.TTZ, texName="t#bar{t}Z", directory=make_dirs( dirs['TTZ'] ))
+
+dirs['TTTT']  = ["TTTT"]
+TTTT = Sample.fromDirectory(name="TTTT", treeName="Events", isData=False, color=color.TTTT, texName="t#bar{t}t#bar{t}", directory=make_dirs( dirs['TTTT'] ))
+
+dirs['TTWZ']  = ["TTWZ"]
+TTWZ = Sample.fromDirectory(name="TTWZ", treeName="Events", isData=False, color=color.TTWZ, texName="t#bar{t}WZ", directory=make_dirs( dirs['TTWZ'] ))
+
+dirs['TTWW']  = ["TTWW"]
+TTWW = Sample.fromDirectory(name="TTWW", treeName="Events", isData=False, color=color.TTWW, texName="t#bar{t}WW", directory=make_dirs( dirs['TTWW'] ))
+
+dirs['TTZZ']  = ["TTZZ"]
+TTZZ = Sample.fromDirectory(name="TTZZ", treeName="Events", isData=False, color=color.TTZZ, texName="t#bar{t}ZZ", directory=make_dirs( dirs['TTZZ'] ))
---
+++ samples/python/nano_mc_private_UL20_Autumn18_postProcessed_dilep.py
@@ -0,0 +1,44 @@
+
+#/scratch-cbe/users/robert.schoefbeck/tWZ/nanoTuples/tWZ_nAODv6_private_v3/2016/dilep:
+#DYJetsToLL_M10to50             DYJetsToLL_M50_HT100to200_ext   DYJetsToLL_M50_HT400to600_comb  DYJetsToLL_M50_LO_ext1_comb          DYJetsToLL_M5to50_HT400to600_comb  TTTT
+#DYJetsToLL_M10to50_LO          DYJetsToLL_M50_HT1200to2500     DYJetsToLL_M50_HT600to800       DYJetsToLL_M50_LO_ext1_comb_lheHT70  DYJetsToLL_M5to50_HT600toInf       TTWW
+#DYJetsToLL_M10to50_LO_lheHT70  DYJetsToLL_M50_HT200to400_comb  DYJetsToLL_M50_HT70to100        DYJetsToLL_M5to50_HT100to200_comb    DYJetsToLL_M5to50_HT70to100        TTWZ
+#DYJetsToLL_M50_ext2            DYJetsToLL_M50_HT2500toInf      DYJetsToLL_M50_HT800to1200      DYJetsToLL_M5to50_HT200to400_comb    TTLep_pow                          TTZZ
+#
+#/scratch-cbe/users/robert.schoefbeck/tWZ/nanoTuples/tWZ_nAODv6_private_v3/2017/dilep:
+#DYJetsToLL_M10to50_LO           DYJetsToLL_M4to50_HT200to400_comb  DYJetsToLL_M50_HT100to200       DYJetsToLL_M50_HT2500toInf  DYJetsToLL_M50_HT70to100    DYJetsToLL_M50_LO_comb_lheHT70  TTWW
+#DYJetsToLL_M10to50_LO_lheHT100  DYJetsToLL_M4to50_HT400to600_comb  DYJetsToLL_M50_HT1200to2500     DYJetsToLL_M50_HT400to600   DYJetsToLL_M50_HT800to1200  TTLep_pow                       TTWZ
+#DYJetsToLL_M4to50_HT100to200    DYJetsToLL_M4to50_HT600toInf_comb  DYJetsToLL_M50_HT200to400_comb  DYJetsToLL_M50_HT600to800   DYJetsToLL_M50_LO_comb      TTTT                            TTZZ
+
+
+
+#DYJetsToLL_M10to50_LO
+#DYJetsToLL_M50_HT1200to2500
+#DYJetsToLL_M50_HT2500toInf
+#DYJetsToLL_M50_HT600to800
+#DYJetsToLL_M50_HT800to1200
+#DYJetsToLL_M50_HT100to200
+#DYJetsToLL_M50_HT200to400
+#DYJetsToLL_M50_HT400to600
+#DYJetsToLL_M50_HT70to100
+#DYJetsToLL_M50_LO_lheHT70
+
+import os
+from RootTools.core.Sample import Sample
+import ROOT
+
+# Logging
+import logging
+logger = logging.getLogger(__name__)
+
+from tttt.samples.color import color
+
+directory_ = "/scratch-cbe/users/robert.schoefbeck/tttt/nanoTuples/tttt_v1/UL2018/dilep/"
+
+def make_dirs( dirs ):
+    return [ os.path.join( directory_, dir_ ) for dir_ in dirs ]
+
+dirs = {}
+
+dirs['TTLep']  = ["TTLep_pow_CP5"]
+TTLep = Sample.fromDirectory(name="TTLep", treeName="Events", isData=False, color=color.TT, texName="t#bar{t}", directory=make_dirs( dirs['TTLep'] ))
