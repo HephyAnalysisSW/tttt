7bd8874b0119e1c2fae6511074dae9e372f5d7f2
Robert Schoefbeck <robert.schoefbeck@cern.ch>
2022-11-16 16:38:57 +0100

    medium nominal btag

--- samples/python/config.py
+++ samples/python/config.py
@@ -3,9 +3,9 @@ location_data_UL2016_preVFP      = "/scratch-cbe/users/robert.schoefbeck/tttt/na
 location_data_UL2017             = "/scratch-cbe/users/robert.schoefbeck/tttt/nanoTuples/tttt_v3/UL2017/dilep/"
 location_data_UL2018             = "/scratch-cbe/users/robert.schoefbeck/tttt/nanoTuples/tttt_v3/UL2018/dilep/"
 
-location_mc_UL2016               = "/scratch-cbe/users/robert.schoefbeck/tttt/nanoTuples/tttt_v4_btag_loose/UL2016/dilep/"
-location_mc_UL2016_preVFP        = "/scratch-cbe/users/robert.schoefbeck/tttt/nanoTuples/tttt_v4_btag_loose/UL2016_preVFP/dilep/"
-location_mc_UL2017               = "/scratch-cbe/users/robert.schoefbeck/tttt/nanoTuples/tttt_v4_btag_loose/UL2017/dilep/"
-location_mc_UL2018               = "/scratch-cbe/users/robert.schoefbeck/tttt/nanoTuples/tttt_v4_btag_loose/UL2018/dilep/"
+location_mc_UL2016               = "/scratch-cbe/users/robert.schoefbeck/tttt/nanoTuples/tttt_v3/UL2016/dilep/"
+location_mc_UL2016_preVFP        = "/scratch-cbe/users/robert.schoefbeck/tttt/nanoTuples/tttt_v3/UL2016_preVFP/dilep/"
+location_mc_UL2017               = "/scratch-cbe/users/robert.schoefbeck/tttt/nanoTuples/tttt_v3/UL2017/dilep/"
+location_mc_UL2018               = "/scratch-cbe/users/robert.schoefbeck/tttt/nanoTuples/tttt_v3/UL2018/dilep/"
 
 lumi_era = {"Run2016" : (16.5)*1000, "Run2016_preVFP" : (19.5)*1000, "Run2017" : (41.5)*1000, "Run2018" : (59.97)*1000}
