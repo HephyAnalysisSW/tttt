8d044d9a16e20c15f204842677e7448bfdd230d5
Robert Schoefbeck <robert.schoefbeck@cern.ch>
2022-11-21 11:33:42 +0100

    check PP

---
+++ postProcessing/checkPostProcessingRootFiles.sh
@@ -0,0 +1,8 @@
+python checkPostProcessingRootFiles.py --createExec --data_directory /scratch-cbe/users/robert.schoefbeck/tttt/nanoTuples/ --file nanoPostProcessing_UL20_Run2016_preVFP.sh
+python checkPostProcessingRootFiles.py --createExec --data_directory /scratch-cbe/users/robert.schoefbeck/tttt/nanoTuples/ --file nanoPostProcessing_UL20_Run2016.sh
+python checkPostProcessingRootFiles.py --createExec --data_directory /scratch-cbe/users/robert.schoefbeck/tttt/nanoTuples/ --file nanoPostProcessing_UL20_Run2017.sh
+python checkPostProcessingRootFiles.py --createExec --data_directory /scratch-cbe/users/robert.schoefbeck/tttt/nanoTuples/ --file nanoPostProcessing_UL20_Run2018.sh
+python checkPostProcessingRootFiles.py --createExec --data_directory /scratch-cbe/users/robert.schoefbeck/tttt/nanoTuples/ --file nanoPostProcessing_UL20_Summer16_preVFP.sh
+python checkPostProcessingRootFiles.py --createExec --data_directory /scratch-cbe/users/robert.schoefbeck/tttt/nanoTuples/ --file nanoPostProcessing_UL20_Summer16.sh
+python checkPostProcessingRootFiles.py --createExec --data_directory /scratch-cbe/users/robert.schoefbeck/tttt/nanoTuples/ --file nanoPostProcessing_UL20_Fall17.sh
+python checkPostProcessingRootFiles.py --createExec --data_directory /scratch-cbe/users/robert.schoefbeck/tttt/nanoTuples/ --file nanoPostProcessing_UL20_Autumn18.sh
