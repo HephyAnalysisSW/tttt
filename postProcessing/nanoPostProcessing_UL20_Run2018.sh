cdd959967b6861696d7ad2c969971d0301a17d01
cristina.giordano <cristina.giordano@oeaw.ac.at>
2022-11-22 10:04:05 +0100

    fix spelling

--- postProcessing/nanoPostProcessing_UL20_Run2018.sh
+++ postProcessing/nanoPostProcessing_UL20_Run2018.sh
@@ -1,19 +1,19 @@
-python nanoPostProcessing.py  --forceProxy --notriggerSelection --addDoubleB --btag_WP loose --skim dilep --era UL2018 --processingEra tttt_v4_btag_loose --sample SingleMuon_Run2018A #SPLIT100
-python nanoPostProcessing.py  --forceProxy --notriggerSelection --addDoubleB --btag_WP loose --skim dilep --era UL2018 --processingEra tttt_v4_btag_loose --sample SingleMuon_Run2018B #SPLIT100
-python nanoPostProcessing.py  --forceProxy --notriggerSelection --addDoubleB --btag_WP loose --skim dilep --era UL2018 --processingEra tttt_v4_btag_loose --sample SingleMuon_Run2018C #SPLIT100
-python nanoPostProcessing.py  --forceProxy --notriggerSelection --addDoubleB --btag_WP loose --skim dilep --era UL2018 --processingEra tttt_v4_btag_loose --sample SingleMuon_Run2018D #SPLIT500
+python nanoPostProcessing.py  --forceProxy --noTriggerSelection --addDoubleB --btag_WP loose --skim dilep --era UL2018 --processingEra tttt_v4_btag_loose --sample SingleMuon_Run2018A #SPLIT100
+python nanoPostProcessing.py  --forceProxy --noTriggerSelection --addDoubleB --btag_WP loose --skim dilep --era UL2018 --processingEra tttt_v4_btag_loose --sample SingleMuon_Run2018B #SPLIT100
+python nanoPostProcessing.py  --forceProxy --noTriggerSelection --addDoubleB --btag_WP loose --skim dilep --era UL2018 --processingEra tttt_v4_btag_loose --sample SingleMuon_Run2018C #SPLIT100
+python nanoPostProcessing.py  --forceProxy --noTriggerSelection --addDoubleB --btag_WP loose --skim dilep --era UL2018 --processingEra tttt_v4_btag_loose --sample SingleMuon_Run2018D #SPLIT500
 
-python nanoPostProcessing.py  --forceProxy --notriggerSelection --addDoubleB --btag_WP loose --skim dilep --era UL2018 --processingEra tttt_v4_btag_loose --sample EGamma_Run2018A #SPLIT100
-python nanoPostProcessing.py  --forceProxy --notriggerSelection --addDoubleB --btag_WP loose --skim dilep --era UL2018 --processingEra tttt_v4_btag_loose --sample EGamma_Run2018B #SPLIT100
-python nanoPostProcessing.py  --forceProxy --notriggerSelection --addDoubleB --btag_WP loose --skim dilep --era UL2018 --processingEra tttt_v4_btag_loose --sample EGamma_Run2018C #SPLIT100
-python nanoPostProcessing.py  --forceProxy --notriggerSelection --addDoubleB --btag_WP loose --skim dilep --era UL2018 --processingEra tttt_v4_btag_loose --sample EGamma_Run2018D #SPLIT500
+python nanoPostProcessing.py  --forceProxy --noTriggerSelection --addDoubleB --btag_WP loose --skim dilep --era UL2018 --processingEra tttt_v4_btag_loose --sample EGamma_Run2018A #SPLIT100
+python nanoPostProcessing.py  --forceProxy --noTriggerSelection --addDoubleB --btag_WP loose --skim dilep --era UL2018 --processingEra tttt_v4_btag_loose --sample EGamma_Run2018B #SPLIT100
+python nanoPostProcessing.py  --forceProxy --noTriggerSelection --addDoubleB --btag_WP loose --skim dilep --era UL2018 --processingEra tttt_v4_btag_loose --sample EGamma_Run2018C #SPLIT100
+python nanoPostProcessing.py  --forceProxy --noTriggerSelection --addDoubleB --btag_WP loose --skim dilep --era UL2018 --processingEra tttt_v4_btag_loose --sample EGamma_Run2018D #SPLIT500
 
-python nanoPostProcessing.py  --forceProxy --notriggerSelection --addDoubleB --btag_WP loose --skim dilep --era UL2018 --processingEra tttt_v4_btag_loose --sample DoubleMuon_Run2018A #SPLIT100
-python nanoPostProcessing.py  --forceProxy --notriggerSelection --addDoubleB --btag_WP loose --skim dilep --era UL2018 --processingEra tttt_v4_btag_loose --sample DoubleMuon_Run2018B #SPLIT100
-python nanoPostProcessing.py  --forceProxy --notriggerSelection --addDoubleB --btag_WP loose --skim dilep --era UL2018 --processingEra tttt_v4_btag_loose --sample DoubleMuon_Run2018C #SPLIT100
-python nanoPostProcessing.py  --forceProxy --notriggerSelection --addDoubleB --btag_WP loose --skim dilep --era UL2018 --processingEra tttt_v4_btag_loose --sample DoubleMuon_Run2018D #SPLIT100
+python nanoPostProcessing.py  --forceProxy --noTriggerSelection --addDoubleB --btag_WP loose --skim dilep --era UL2018 --processingEra tttt_v4_btag_loose --sample DoubleMuon_Run2018A #SPLIT100
+python nanoPostProcessing.py  --forceProxy --noTriggerSelection --addDoubleB --btag_WP loose --skim dilep --era UL2018 --processingEra tttt_v4_btag_loose --sample DoubleMuon_Run2018B #SPLIT100
+python nanoPostProcessing.py  --forceProxy --noTriggerSelection --addDoubleB --btag_WP loose --skim dilep --era UL2018 --processingEra tttt_v4_btag_loose --sample DoubleMuon_Run2018C #SPLIT100
+python nanoPostProcessing.py  --forceProxy --noTriggerSelection --addDoubleB --btag_WP loose --skim dilep --era UL2018 --processingEra tttt_v4_btag_loose --sample DoubleMuon_Run2018D #SPLIT100
 
-python nanoPostProcessing.py  --forceProxy --notriggerSelection --addDoubleB --btag_WP loose --skim dilep --era UL2018 --processingEra tttt_v4_btag_loose --sample MuonEG_Run2018A #SPLIT100
-python nanoPostProcessing.py  --forceProxy --notriggerSelection --addDoubleB --btag_WP loose --skim dilep --era UL2018 --processingEra tttt_v4_btag_loose --sample MuonEG_Run2018B #SPLIT100
-python nanoPostProcessing.py  --forceProxy --notriggerSelection --addDoubleB --btag_WP loose --skim dilep --era UL2018 --processingEra tttt_v4_btag_loose --sample MuonEG_Run2018C #SPLIT100
-python nanoPostProcessing.py  --forceProxy --notriggerSelection --addDoubleB --btag_WP loose --skim dilep --era UL2018 --processingEra tttt_v4_btag_loose --sample MuonEG_Run2018D #SPLIT100
+python nanoPostProcessing.py  --forceProxy --noTriggerSelection --addDoubleB --btag_WP loose --skim dilep --era UL2018 --processingEra tttt_v4_btag_loose --sample MuonEG_Run2018A #SPLIT100
+python nanoPostProcessing.py  --forceProxy --noTriggerSelection --addDoubleB --btag_WP loose --skim dilep --era UL2018 --processingEra tttt_v4_btag_loose --sample MuonEG_Run2018B #SPLIT100
+python nanoPostProcessing.py  --forceProxy --noTriggerSelection --addDoubleB --btag_WP loose --skim dilep --era UL2018 --processingEra tttt_v4_btag_loose --sample MuonEG_Run2018C #SPLIT100
+python nanoPostProcessing.py  --forceProxy --noTriggerSelection --addDoubleB --btag_WP loose --skim dilep --era UL2018 --processingEra tttt_v4_btag_loose --sample MuonEG_Run2018D #SPLIT100
