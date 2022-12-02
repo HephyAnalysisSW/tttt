adf88b285d1a7190375ca629fa86a3b659f78f38
Robert Schoefbeck <robert.schoefbeck@cern.ch>
2022-12-02 10:17:07 +0100

    debugging

--- plots/plotsRobert/analysis/analysis-2l.py
+++ plots/plotsRobert/analysis/analysis-2l.py
@@ -51,24 +51,23 @@ if args.noData:args.plot_directory += "_noData"
 from tttt.samples.nano_private_UL20_RunII_postProcessed_dilep import *
 #from TMB.Samples.nanoTuples_RunII_nanoAODv6_dilep_pp import *
 
-
 # Split dileptonic TTBar into three different contributions
-sample_TTLep = TTLepbb
+sample_TTLep = TTLep
 # ttbar gen classification: https://github.com/cms-top/cmssw/blob/topNanoV6_from-CMSSW_10_2_18/TopQuarkAnalysis/TopTools/plugins/GenTtbarCategorizer.cc
 TTLep_bb    = copy.deepcopy( sample_TTLep )
 TTLep_bb.name = "TTLep_bb"
 TTLep_bb.texName = "t#bar{t}b#bar{b}"
 TTLep_bb.color   = ROOT.kRed + 2 
-TTLep_bb.setSelectionString( "genTtbarId%100>=50&&overlapRemoval" )
+TTLep_bb.setSelectionString( "genTtbarId%100>=50" )
 TTLep_cc    = copy.deepcopy( sample_TTLep )
 TTLep_cc.name = "TTLep_cc"
 TTLep_cc.texName = "t#bar{t}c#bar{c}" 
 TTLep_cc.color   = ROOT.kRed - 3 
-TTLep_cc.setSelectionString( "genTtbarId%100>=40&&genTtbarId%100<50&&overlapRemoval" )
+TTLep_cc.setSelectionString( "genTtbarId%100>=40&&genTtbarId%100<50" )
 TTLep_other = copy.deepcopy( sample_TTLep )
 TTLep_other.name = "TTLep_other"
 TTLep_other.texName = "t#bar{t} + light j." 
-TTLep_other.setSelectionString( "genTtbarId%100<40&&overlapRemoval" )
+TTLep_other.setSelectionString( "genTtbarId%100<40" )
 
 # group all the simulated backgroundsamples 
 mc = [ TTLep_bb, TTLep_cc, TTLep_other, ST, TTW, TTH, TTZ, TTTT] 
