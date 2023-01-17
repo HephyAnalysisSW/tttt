900adc73cd9de645f69adb6b31e5e7078b10d61f
Lena Wild <lena.wild@oeaw.ac.at>
2023-01-17 14:57:12 +0100

    plots lena

---
+++ plots/plotsLena/gen/skim_plots.sh
@@ -0,0 +1,11 @@
+
+python skim_plots.py --plot_directory gen_v1 --WCval 1.0 --WCval_FI 1.0 --sample TTTT_MS --WC ctt
+#python skim_plots.py --plot_directory gen_v10 --WCval 1.0 --WCval_FI 1.0 --sample ZZ --ZZ --selection nZ2p-VBS --WC cpq3i
+#python skim_plots.py --plot_directory gen_v10 --WCval 1.0 --WCval_FI 1.0 --sample ZZ --ZZ --selection nZ2p-VBS --WC cpqMi
+#python skim_plots.py --plot_directory gen_v10 --WCval 1.0 --WCval_FI 1.0 --sample ZZ --ZZ --selection nZ2p-VBS --WC cpu
+#python skim_plots.py --plot_directory gen_v10 --WCval 1.0 --WCval_FI 1.0 --sample ZZ --ZZ --selection nZ2p-VBS --WC cpt
+#python skim_plots.py --plot_directory gen_v10 --WCval 1.0 --WCval_FI 1.0 --sample ZZ --ZZ --selection nZ2p-VBS --WC cpQ3
+#python skim_plots.py --plot_directory gen_v10 --WCval 1.0 --WCval_FI 1.0 --sample ZZ --ZZ --selection nZ2p-VBS --WC cpQM
+#python skim_plots.py --plot_directory gen_v10 --WCval 1.0 --WCval_FI 1.0 --sample ZZ --ZZ --selection nZ2p-VBS --WC cpb
+#python skim_plots.py --plot_directory gen_v10 --WCval 1.0 --WCval_FI 1.0 --sample ZZ --ZZ --selection nZ2p-VBS --WC cpd
+
