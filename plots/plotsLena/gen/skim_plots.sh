d78d0c134523fd97bbd99565f5497ff079110845
Lena Wild <lena.wild@oeaw.ac.at>
2023-01-23 11:20:29 +0100

    update sh

--- plots/plotsLena/gen/skim_plots.sh
+++ plots/plotsLena/gen/skim_plots.sh
@@ -1,11 +1,28 @@
 
-python skim_plots.py --plot_directory gen_v1 --WCval 1.0 --WCval_FI 1.0 --sample TTTT_MS --WC ctt
-#python skim_plots.py --plot_directory gen_v10 --WCval 1.0 --WCval_FI 1.0 --sample ZZ --ZZ --selection nZ2p-VBS --WC cpq3i
-#python skim_plots.py --plot_directory gen_v10 --WCval 1.0 --WCval_FI 1.0 --sample ZZ --ZZ --selection nZ2p-VBS --WC cpqMi
-#python skim_plots.py --plot_directory gen_v10 --WCval 1.0 --WCval_FI 1.0 --sample ZZ --ZZ --selection nZ2p-VBS --WC cpu
-#python skim_plots.py --plot_directory gen_v10 --WCval 1.0 --WCval_FI 1.0 --sample ZZ --ZZ --selection nZ2p-VBS --WC cpt
-#python skim_plots.py --plot_directory gen_v10 --WCval 1.0 --WCval_FI 1.0 --sample ZZ --ZZ --selection nZ2p-VBS --WC cpQ3
-#python skim_plots.py --plot_directory gen_v10 --WCval 1.0 --WCval_FI 1.0 --sample ZZ --ZZ --selection nZ2p-VBS --WC cpQM
-#python skim_plots.py --plot_directory gen_v10 --WCval 1.0 --WCval_FI 1.0 --sample ZZ --ZZ --selection nZ2p-VBS --WC cpb
-#python skim_plots.py --plot_directory gen_v10 --WCval 1.0 --WCval_FI 1.0 --sample ZZ --ZZ --selection nZ2p-VBS --WC cpd
-
+#python skim_plots.py --plot_directory gen_v1 --WCval 1.0 --WCval_FI 1.0 --sample TTTT_MS --WC ctt
+python skim_plots.py --plot_directory Gen_v1 --WCval 1.0 --WCval_FI 1.0 --sample TTTT_MS --WC ctt cQQ1 cQQ8 cQt1 cQt8 ctHRe ctHIm
+python skim_plots.py --plot_directory Gen_v1 --WCval 1.0 --WCval_FI 1.0 --sample TTbb_MS --WC ctt cQQ1 cQQ8 cQt1 cQt8 ctHRe ctHIm ctb1 ctb8 cQb1 cQb8 cQtQb1Re cQtQb8Re cQtQb1Im cQtQb8Im
+#
+python skim_plots.py --plot_directory Gen_v1_Single --WCval 1.0 --WCval_FI 1.0 --sample TTTT_MS --WC ctt  
+python skim_plots.py --plot_directory Gen_v1_Single --WCval 1.0 --WCval_FI 1.0 --sample TTTT_MS --WC cQQ1 
+python skim_plots.py --plot_directory Gen_v1_Single --WCval 1.0 --WCval_FI 1.0 --sample TTTT_MS --WC cQQ8 
+python skim_plots.py --plot_directory Gen_v1_Single --WCval 1.0 --WCval_FI 1.0 --sample TTTT_MS --WC cQt1 
+python skim_plots.py --plot_directory Gen_v1_Single --WCval 1.0 --WCval_FI 1.0 --sample TTTT_MS --WC cQt8 
+python skim_plots.py --plot_directory Gen_v1_Single --WCval 1.0 --WCval_FI 1.0 --sample TTTT_MS --WC ctHRe
+python skim_plots.py --plot_directory Gen_v1_Single --WCval 1.0 --WCval_FI 1.0 --sample TTTT_MS --WC ctHIm
+#
+python skim_plots.py --plot_directory Gen_v1_Single --WCval 1.0 --WCval_FI 1.0 --sample TTbb_MS --WC ctt 
+python skim_plots.py --plot_directory Gen_v1_Single --WCval 1.0 --WCval_FI 1.0 --sample TTbb_MS --WC cQQ1
+python skim_plots.py --plot_directory Gen_v1_Single --WCval 1.0 --WCval_FI 1.0 --sample TTbb_MS --WC cQQ8
+python skim_plots.py --plot_directory Gen_v1_Single --WCval 1.0 --WCval_FI 1.0 --sample TTbb_MS --WC cQt1
+python skim_plots.py --plot_directory Gen_v1_Single --WCval 1.0 --WCval_FI 1.0 --sample TTbb_MS --WC cQt8
+python skim_plots.py --plot_directory Gen_v1_Single --WCval 1.0 --WCval_FI 1.0 --sample TTbb_MS --WC ctHRe
+python skim_plots.py --plot_directory Gen_v1_Single --WCval 1.0 --WCval_FI 1.0 --sample TTbb_MS --WC ctHIm
+python skim_plots.py --plot_directory Gen_v1_Single --WCval 1.0 --WCval_FI 1.0 --sample TTbb_MS --WC ctb1 
+python skim_plots.py --plot_directory Gen_v1_Single --WCval 1.0 --WCval_FI 1.0 --sample TTbb_MS --WC ctb8 
+python skim_plots.py --plot_directory Gen_v1_Single --WCval 1.0 --WCval_FI 1.0 --sample TTbb_MS --WC cQb1 
+python skim_plots.py --plot_directory Gen_v1_Single --WCval 1.0 --WCval_FI 1.0 --sample TTbb_MS --WC cQb8 
+python skim_plots.py --plot_directory Gen_v1_Single --WCval 1.0 --WCval_FI 1.0 --sample TTbb_MS --WC cQtQb1Re 
+python skim_plots.py --plot_directory Gen_v1_Single --WCval 1.0 --WCval_FI 1.0 --sample TTbb_MS --WC cQtQb8Re 
+python skim_plots.py --plot_directory Gen_v1_Single --WCval 1.0 --WCval_FI 1.0 --sample TTbb_MS --WC cQtQb1Im 
+python skim_plots.py --plot_directory Gen_v1_Single --WCval 1.0 --WCval_FI 1.0 --sample TTbb_MS --WC cQtQb8Im
