527484568534d5b7bf10572baf43125067337db3
Lena Wild <lena.wild@oeaw.ac.at>
2023-01-24 15:44:43 +0100

    delphes config

--- postProcessing/genPostProcessing.sh
+++ postProcessing/genPostProcessing.sh
@@ -1,3 +1,3 @@
 #!/bin/sh
-python genPostProcessing.py --targetDir v1 --overwrite --addReweights --logLevel INFO --interpolationOrder 2 --delphesEra RunII --sample TTTT_MS #SPLIT493
-#python genPostProcessing.py --targetDir v1 --overwrite --addReweights --logLevel INFO --interpolationOrder 2 --delphesEra RunII --sample TTbb_MS #SPLIT200
+python genPostProcessing.py --targetDir v2 --overwrite --addReweights --logLevel INFO --interpolationOrder 2 --delphesEra RunII --sample TTTT_MS #SPLIT493
+python genPostProcessing.py --targetDir v2 --overwrite --addReweights --logLevel INFO --interpolationOrder 2 --delphesEra RunII --sample TTbb_MS #SPLIT497
