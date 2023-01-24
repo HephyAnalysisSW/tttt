005be4ae08f403be24bbc17b3d282bdfe748358b
Lena Wild <lena.wild@oeaw.ac.at>
2023-01-24 12:10:38 +0100

    new v2

--- samples/python/GEN_EFT_postProcessed.py
+++ samples/python/GEN_EFT_postProcessed.py
@@ -7,14 +7,20 @@ import logging
 logger = logging.getLogger(__name__)
 
 from tttt.samples.color import color
-dir = "/scratch-cbe/users/robert.schoefbeck/tttt/nanoTuples/gen/v1/"
+#dir = "/scratch-cbe/users/robert.schoefbeck/tttt/nanoTuples/gen/v1/"
+dir = "/scratch-cbe/users/lena.wild/tttt/nanoTuples/gen/v2/"
 
 TTTT_MS = Sample.fromDirectory("TTTT_MS", os.path.join( dir, "TTTT_MS" ))
 TTTT_MS.reweight_pkl = "/eos/vbc/group/cms/robert.schoefbeck/gridpacks/4top/TTTT_MS_reweight_card.pkl" 
 TTTT_MS.color = color.TTTT
 TTTT_MS.objects = ['t']
 
-allSamples = [ TTTT_MS ]
+TTbb_MS = Sample.fromDirectory("TTbb_MS", os.path.join( dir, "TTbb_MS" ))
+TTbb_MS.reweight_pkl = "/eos/vbc/group/cms/robert.schoefbeck/gridpacks/4top/TTbb_MS_reweight_card.pkl" 
+TTbb_MS.color = color.TTbb
+TTbb_MS.objects = ['t']
+
+allSamples = [ TTTT_MS, TTbb_MS ]
 
 for s in allSamples:
   s.isData  = False
