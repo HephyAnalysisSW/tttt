3ffcf78197db2780876185cd1cb61e5d2c11bbcc
Lena Wild <lena.wild@oeaw.ac.at>
2023-01-31 12:36:14 +0100

    for delphes

---
+++ MVA/python/make_ntuple_delphes.sh
@@ -0,0 +1,2 @@
+python make_ntuple_delphes.py  --output /eos/vbc/group/cms/lena.wild/tttt/training-ntuples-tttt --selection dilep-met30-njet4p-btag2p --config ttbb_2l --sample TTTT_MS
+python make_ntuple_delphes.py  --output /eos/vbc/group/cms/lena.wild/tttt/training-ntuples-tttt --selection dilep-met30-njet4p-btag2p --config ttbb_2l --sample TTbb_MS
