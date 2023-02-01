b80f2b4571f0f5f7221d25f9e74e44b37e6768fd
Lwildd <114661709+Lwildd@users.noreply.github.com>
2023-02-01 14:51:57 +0100

    Delete make_ntuple_delphes.sh

--- MVA/python/make_ntuple_delphes.sh
+++
@@ -1,2 +0,0 @@
-python make_ntuple_delphes.py  --output /eos/vbc/group/cms/lena.wild/tttt/training-ntuples-tttt --selection dilep-met30-njet4p-btag2p --config ttbb_2l --sample TTTT_MS
-python make_ntuple_delphes.py  --output /eos/vbc/group/cms/lena.wild/tttt/training-ntuples-tttt --selection dilep-met30-njet4p-btag2p --config ttbb_2l --sample TTbb_MS
