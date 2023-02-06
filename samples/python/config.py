c7d38551f3d4a6f878358624b33388dd75d895d4
Maryam Shooshtari <mary.shooshtari@gmail.com>
2023-02-06 17:04:04 +0100

    adding central samples including DY and DiBoson

--- samples/python/config.py
+++ samples/python/config.py
@@ -36,14 +36,42 @@ elif os.environ["USER"] in ["robert.schoefbeck"]:
 
 
 elif os.environ["USER"] in ["maryam.shooshtari"]:
-    location_data_UL2016             = "/scratch-cbe/users/maryam.shooshtari/tttt/nanoTuples/tttt_v5/UL2016/dilep/"
-    location_data_UL2016_preVFP      = "/scratch-cbe/users/maryam.shooshtari/tttt/nanoTuples/tttt_v5/UL2016_preVFP/dilep/"
-    location_data_UL2017             = "/scratch-cbe/users/maryam.shooshtari/tttt/nanoTuples/tttt_v5/UL2017/dilep/"
-    location_data_UL2018             = "/scratch-cbe/users/maryam.shooshtari/tttt/nanoTuples/tttt_v5/UL2018/dilep/"
+    location_data_UL2016             = "/scratch-cbe/users/robert.schoefbeck/tttt/nanoTuples/tttt_v6/UL2016/dilep-ht500/"
+    location_data_UL2016_preVFP      = "/scratch-cbe/users/robert.schoefbeck/tttt/nanoTuples/tttt_v6/UL2016_preVFP/dilep-ht500/"
+    location_data_UL2017             = "/scratch-cbe/users/robert.schoefbeck/tttt/nanoTuples/tttt_v6/UL2017/dilep-ht500/"
+    location_data_UL2018             = "/scratch-cbe/users/robert.schoefbeck/tttt/nanoTuples/tttt_v6/UL2018/dilep-ht500/"
+    
+    location_mc_UL2016               = "/scratch-cbe/users/robert.schoefbeck/tttt/nanoTuples/tttt_v6/UL2016/dilep-ht500/"
+    location_mc_UL2016_preVFP        = "/scratch-cbe/users/robert.schoefbeck/tttt/nanoTuples/tttt_v6/UL2016_preVFP/dilep-ht500/"
+    location_mc_UL2017               = "/scratch-cbe/users/robert.schoefbeck/tttt/nanoTuples/tttt_v6/UL2017/dilep-ht500/"
+    location_mc_UL2018               = "/scratch-cbe/users/robert.schoefbeck/tttt/nanoTuples/tttt_v6/UL2018/dilep-ht500/"
+    
+    #Maryam's v6 is using central samples
+    location_mc_UL2016_central               = "/scratch-cbe/users/maryam.shooshtari/tttt/nanoTuples/tttt_v6/UL2016/dilep-ht500/"
+    location_mc_UL2016_preVFP_central        = "/scratch-cbe/users/maryam.shooshtari/tttt/nanoTuples/tttt_v6/UL2016_preVFP/dilep-ht500/"
+    location_mc_UL2017_central               = "/scratch-cbe/users/maryam.shooshtari/tttt/nanoTuples/tttt_v6/UL2017/dilep-ht500/"
+    location_mc_UL2018_central               = "/scratch-cbe/users/maryam.shooshtari/tttt/nanoTuples/tttt_v6/UL2018/dilep-ht500/"
+    
+  
+  
+  
+  
+  
+  
+  
+  
+  
+  
+  
+  
+  
+  
+  
+  
 
-    location_mc_UL2016               = "/scratch-cbe/users/maryam.shooshtari/tttt/nanoTuples/tttt_v5/UL2016/dilep/"
-    location_mc_UL2016_preVFP        = "/scratch-cbe/users/maryam.shooshtari/tttt/nanoTuples/tttt_v5/UL2016_preVFP/dilep/"
-    location_mc_UL2017               = "/scratch-cbe/users/maryam.shooshtari/tttt/nanoTuples/tttt_v5/UL2017/dilep/"
-    location_mc_UL2018               = "/scratch-cbe/users/maryam.shooshtari/tttt/nanoTuples/tttt_v5/UL2018/dilep/"
-
+  
+  
+  
+  
+  
 lumi_era = {"Run2016" : (16.5)*1000, "Run2016_preVFP" : (19.5)*1000, "Run2017" : (41.5)*1000, "Run2018" : (59.97)*1000}
