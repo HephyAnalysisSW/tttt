8ef2fd7976176256193cbb3e186e3c37cee3e688
Lena Wild <lena.wild@oeaw.ac.at>
2023-02-13 11:21:24 +0100

    changes

--- plots/plotsLena/delphes/plots.sh
+++ plots/plotsLena/delphes/plots.sh
@@ -1,4 +1,4 @@
 # python plots.py  --signal TTTT_MS --plot_directory delphes_v2
 # python plots.py  --signal TTbb_MS --plot_directory delphes_v2
-python plots.py  --signal TTTT_MS --plot_directory delphes_v3 --selection dilep-met30-njet4p-btag2p
-python plots.py  --signal TTbb_MS --plot_directory delphes_v3 --selection dilep-met30-njet4p-btag2p
+python plots.py  --signal TTTT_MS --plot_directory delphes_v4 --selection dilep-met30-njet4p-btag2p
+python plots.py  --signal TTbb_MS --plot_directory delphes_v4 --selection dilep-met30-njet4p-btag2p
