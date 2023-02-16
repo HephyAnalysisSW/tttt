ec6b2dd6e26abcd28028013f2894f669a4b0cca6
Lena Wild <lena.wild@oeaw.ac.at>
2023-02-16 15:49:48 +0100

    new

--- postProcessing/genPostProcessing.sh
+++ postProcessing/genPostProcessing.sh
@@ -1,3 +1,5 @@
 #!/bin/sh
-python genPostProcessing.py --targetDir v2 --overwrite --addReweights --logLevel INFO --interpolationOrder 2 --delphesEra RunII --sample TTTT_MS #SPLIT493
-python genPostProcessing.py --targetDir v2 --overwrite --addReweights --logLevel INFO --interpolationOrder 2 --delphesEra RunII --sample TTbb_MS #SPLIT497
+# python genPostProcessing.py --targetDir v2 --overwrite --addReweights --logLevel INFO --interpolationOrder 2 --delphesEra RunII --sample TTTT_MS #SPLIT493
+# python genPostProcessing.py --targetDir v2 --overwrite --addReweights --logLevel INFO --interpolationOrder 2 --delphesEra RunII --sample TTbb_MS #SPLIT497
+python genPostProcessing.py --targetDir v4 --overwrite --addReweights --logLevel INFO --interpolationOrder 2 --delphesEra RunII --sample TTTT_MS #SPLIT493
+python genPostProcessing.py --targetDir v4 --overwrite --addReweights --logLevel INFO --interpolationOrder 2 --delphesEra RunII --sample TTbb_MS #SPLIT497
