4f495a71b231065c89f4b36ace9022f0cbfa1590
Lena Wild <lena.wild@oeaw.ac.at>
2023-02-28 12:59:35 +0100

    updates

--- postProcessing/genPostProcessing.sh
+++ postProcessing/genPostProcessing.sh
@@ -3,5 +3,5 @@
 # python genPostProcessing.py --targetDir v2 --overwrite --addReweights --logLevel INFO --interpolationOrder 2 --delphesEra RunII --sample TTbb_MS #SPLIT497
 # python genPostProcessing.py --targetDir v4 --overwrite --addReweights --logLevel INFO --interpolationOrder 2 --delphesEra RunII --sample TTTT_MS #SPLIT493
 # python genPostProcessing.py --targetDir v4 --overwrite --addReweights --logLevel INFO --interpolationOrder 2 --delphesEra RunII --sample TTbb_MS #SPLIT497
-python genPostProcessing.py --targetDir v6 --overwrite --addReweights --logLevel INFO --interpolationOrder 2 --delphesEra RunII --sample TTTT_MS --config ttbb_2l --add_training_vars --trainingCoefficients ctt cQQ1 cQQ8 cQt1 cQt8 ctHRe ctHIm #SPLIT493
-# python genPostProcessing.py --targetDir v6 --overwrite --addReweights --logLevel INFO --interpolationOrder 2 --delphesEra RunII --sample TTbb_MS --config ttbb_2l --add_training_vars #SPLIT497
+#python genPostProcessing.py --targetDir v6 --overwrite --addReweights --logLevel INFO --interpolationOrder 2 --delphesEra RunII --sample TTTT_MS --config ttbb_2l --add_training_vars --trainingCoefficients ctt cQQ1 cQQ8 cQt1 cQt8 ctHRe ctHIm #SPLIT493
+python genPostProcessing.py --targetDir v6 --overwrite --addReweights --logLevel INFO --interpolationOrder 2 --delphesEra RunII --sample TTbb_MS --config ttbb_2l --add_training_vars #SPLIT497
