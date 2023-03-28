d2778059680291445b36de92a8a6f920f189c66a
Lena Wild <lena.wild@oeaw.ac.at>
2023-03-28 08:10:23 +0200

    scripts for calculating sum of all weights in sample

---
+++ samples/python/GEN_EFT_postProcessed_weights.py
@@ -0,0 +1,26 @@
+import copy, os, sys
+from RootTools.core.Sample import Sample
+import ROOT
+
+# Logging
+import logging
+logger = logging.getLogger(__name__)
+
+from tttt.samples.color import color
+#dir = "/scratch-cbe/users/robert.schoefbeck/tttt/nanoTuples/gen/v1/"
+# dir = "/scratch-cbe/users/lena.wild/tttt/nanoTuples/gen/v1__small/"
+dir = "/scratch-cbe/users/lena.wild/tttt/nanoTuples/gen/v7w_weightsum/"
+
+TTTT_MS = Sample.fromDirectory("TTTT_MS", os.path.join( dir, "TTTT_MS" ))
+
+
+TTbb_MS = Sample.fromDirectory("TTbb_MS", os.path.join( dir, "TTbb_MS" ))
+
+
+TT_2L   = Sample.fromDirectory("TT_2L", os.path.join( dir, "TT_2L" ))
+
+
+allSamples = [ TTTT_MS, TTbb_MS, TT_2L ]
+
+for s in allSamples:
+  s.isData  = False
