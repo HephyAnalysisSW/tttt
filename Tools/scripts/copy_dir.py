52a66ec7f15505b89b8e45a3bf074b8f1c615552
cristina.giordano <cristina.giordano@oeaw.ac.at>
2023-05-15 11:00:02 +0200

    fix logic

--- Tools/scripts/copy_dir.py
+++ Tools/scripts/copy_dir.py
@@ -1,5 +1,5 @@
 #!/usr/bin/env python
-''' Copy a directory and apply extra selection 
+''' Copy a directory and apply extra selection
 '''
 #
 # Standard imports and batch mode
@@ -12,6 +12,7 @@ import os
 from RootTools.core.standard             import *
 
 from tttt.Tools.cutInterpreter           import cutInterpreter
+from tttt.Tools.user                     import *
 
 # Arguments
 import argparse
@@ -20,29 +21,29 @@ argParser.add_argument('--logLevel',       action='store',      default='INFO', 
 #argParser.add_argument('--small',                             action='store_true', help='Run only on a small subset of the data?')
 argParser.add_argument('--overwrite',      action='store_true', help='Overwrite?')
 argParser.add_argument('--source',         action='store', default='/scratch-cbe/users/maryam.shooshtari/tttt/nanoTuples/tttt_v7/UL2016/dilep-ht500/')
-argParser.add_argument('--target',         action='store', default='/scratch-cbe/users/robert.schoefbeck/tttt/nanoTuples/tttt_v7/UL2016/')
+argParser.add_argument('--target',         action='store', default='/scratch-cbe/users/$USER/tttt/nanoTuples/tttt_v7/UL2016')
+#argParser.add_argument('--version',        action='store', default='tttt_v7', help='which version to copy to?')
 argParser.add_argument('--target_subdir',  action='store', default=None, help='If specified, will write to "target/target_subdir" instead if "target/source_subdir-selection".')
 argParser.add_argument('--selection',      action='store', default='ht1000')
-argParser.add_argument('--sample',         action='store', default=None, help='Specify sample subdir after args.target_subdir')
+#argParser.add_argument('--sample',         action='store', default=None, help='Specify sample subdir after args.target_subdir')
 args = argParser.parse_args()
 
 # Logger
-import TMB.Tools.logger as logger
+import tttt.Tools.logger as logger
 import RootTools.core.logger as logger_rt
 logger    = logger.get_logger(   args.logLevel, logFile = None)
 logger_rt = logger_rt.get_logger(args.logLevel, logFile = None)
 
 
 if args.target_subdir is not None:
-    target = os.path.join( args.target, args.target_subdir, args.sample )
+    target = os.path.join( args.target, args.target_subdir)
 else:
-    target = os.path.join( args.target, os.path.dirname(args.source).split('/')[-1]+'-'+args.selection, args.sample) 
+    target = os.path.join( args.target, os.path.dirname(args.source).split('/')[-1]+'-'+args.selection)
 
 for i_entry, entry in enumerate( os.listdir(args.source) ):
     sample_dir = os.path.join(args.source, entry)
     if os.path.isdir(sample_dir):
         sample = Sample.fromDirectory( "s%i"%i_entry, directory = sample_dir )
         #print(sample.name, len(sample.files))
-        if entry == args.sample:
-            sample.copy_files( target, selection=cutInterpreter.cutString(args.selection))#, overwrite=args.overwrite)
-
+        #if entry == args.sample:
+        sample.copy_files( os.path.join(target, entry), selection=cutInterpreter.cutString(args.selection))#, overwrite=args.overwrite)
