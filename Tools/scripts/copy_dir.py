b85fcf60036dd6c656216ad7adbf527978301742
cristina.giordano <cristina.giordano@oeaw.ac.at>
2023-05-15 09:53:45 +0200

    fix logic

--- Tools/scripts/copy_dir.py
+++ Tools/scripts/copy_dir.py
@@ -23,6 +23,7 @@ argParser.add_argument('--source',         action='store', default='/scratch-cbe
 argParser.add_argument('--target',         action='store', default='/scratch-cbe/users/robert.schoefbeck/tttt/nanoTuples/tttt_v7/UL2016/')
 argParser.add_argument('--target_subdir',  action='store', default=None, help='If specified, will write to "target/target_subdir" instead if "target/source_subdir-selection".')
 argParser.add_argument('--selection',      action='store', default='ht1000')
+argParser.add_argument('--sample',         action='store', default=None, help='Specify sample subdir after args.target_subdir')
 args = argParser.parse_args()
 
 # Logger
@@ -33,14 +34,15 @@ logger_rt = logger_rt.get_logger(args.logLevel, logFile = None)
 
 
 if args.target_subdir is not None:
-    target = os.path.join( args.target, args.target_subdir )
+    target = os.path.join( args.target, args.target_subdir, args.sample )
 else:
-    target = os.path.join( args.target, os.path.dirname(args.source).split('/')[-1]+'-'+args.selection) 
+    target = os.path.join( args.target, os.path.dirname(args.source).split('/')[-1]+'-'+args.selection, args.sample) 
 
 for i_entry, entry in enumerate( os.listdir(args.source) ):
     sample_dir = os.path.join(args.source, entry)
     if os.path.isdir(sample_dir):
         sample = Sample.fromDirectory( "s%i"%i_entry, directory = sample_dir )
         #print(sample.name, len(sample.files))
-        sample.copy_files( target, selection=cutInterpreter.cutString(args.selection), overwrite=args.overwrite)
+        if entry == args.sample:
+            sample.copy_files( target, selection=cutInterpreter.cutString(args.selection))#, overwrite=args.overwrite)
 
