b9a626460382c80ed45ccb058e80b8670d87f107
cristina.giordano <cristina.giordano@oeaw.ac.at>
2023-06-06 10:24:44 +0200

    add split in eras (again)

--- samples/python/config.py
+++ samples/python/config.py
@@ -11,15 +11,15 @@ if os.environ["USER"] in ["cristina.giordano", "lena.wild"]:
     location_mc_UL2017_trilep               = "/scratch-cbe/users/cristina.giordano/tttt/nanoTuples/tttt_v8/UL2017/trilep/"
     location_mc_UL2018_trilep               = "/scratch-cbe/users/cristina.giordano/tttt/nanoTuples/tttt_v8/UL2018/trilep/"
 
-    location_data_UL2016             = "/scratch-cbe/users/maryam.shooshtari/tttt/nanoTuples/tttt_v7/UL2016/dilep-ht500/"
-    location_data_UL2016_preVFP      = "/scratch-cbe/users/maryam.shooshtari/tttt/nanoTuples/tttt_v7/UL2016_preVFP/dilep-ht500/"
-    location_data_UL2017             = "/scratch-cbe/users/maryam.shooshtari/tttt/nanoTuples/tttt_v7/UL2017/dilep-ht500/"
-    location_data_UL2018             = "/scratch-cbe/users/maryam.shooshtari/tttt/nanoTuples/tttt_v7/UL2018/dilep-ht500/"
+    location_data_UL2016             = "/scratch-cbe/users/robert.schoefbeck/tttt/nanoTuples/tttt_v8/UL2016/dilep-nlepFO2p-ht500/"
+    location_data_UL2016_preVFP      = "/scratch-cbe/users/robert.schoefbeck/tttt/nanoTuples/tttt_v8/UL2016_preVFP/dilep-nlepFO2p-ht500/"
+    location_data_UL2017             = "/scratch-cbe/users/robert.schoefbeck/tttt/nanoTuples/tttt_v8/UL2017/dilep-nlepFO2p-ht500/"
+    location_data_UL2018             = "/scratch-cbe/users/robert.schoefbeck/tttt/nanoTuples/tttt_v8/UL2018/dilep-nlepFO2p-ht500/"
 
-    location_mc_UL2016               = "/scratch-cbe/users/maryam.shooshtari/tttt/nanoTuples/tttt_v7/UL2016/dilep-ht500/"
-    location_mc_UL2016_preVFP        = "/scratch-cbe/users/maryam.shooshtari/tttt/nanoTuples/tttt_v7/UL2016_preVFP/dilep-ht500/"
-    location_mc_UL2017               = "/scratch-cbe/users/maryam.shooshtari/tttt/nanoTuples/tttt_v7/UL2017/dilep-ht500/"
-    location_mc_UL2018               = "/scratch-cbe/users/maryam.shooshtari/tttt/nanoTuples/tttt_v7/UL2018/dilep-ht500/"
+    location_mc_UL2016               = "/scratch-cbe/users/maryam.shooshtari/tttt/nanoTuples/tttt_v8/UL2016/dilep/"
+    location_mc_UL2016_preVFP        = "/scratch-cbe/users/maryam.shooshtari/tttt/nanoTuples/tttt_v8/UL2016_preVFP/dilep/"
+    location_mc_UL2017               = "/scratch-cbe/users/maryam.shooshtari/tttt/nanoTuples/tttt_v8/UL2017/dilep/"
+    location_mc_UL2018               = "/scratch-cbe/users/maryam.shooshtari/tttt/nanoTuples/tttt_v8/UL2018/dilep-dilep/"
 
 elif os.environ["USER"] in ["robert.schoefbeck"]:
     location_data_UL2016             = "/scratch-cbe/users/maryam.shooshtari/tttt/nanoTuples/tttt_v7/UL2016/dilep-ht500/"
