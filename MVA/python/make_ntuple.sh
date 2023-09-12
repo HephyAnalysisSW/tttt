08d7d05606d54e9f85ddc9cc74096287fa8c8372
Robert Schoefbeck <robert.schoefbeck@cern.ch>
2023-09-12 22:43:39 +0200

    rebin

--- MVA/python/make_ntuple.sh
+++ MVA/python/make_ntuple.sh
@@ -25,3 +25,6 @@ python make_ntuple.py  --output /eos/vbc/group/cms/$USER/tttt/training-ntuples-t
 # python make_ntuple.py  --output /eos/vbc/group/cms/$USER/tttt/training-ntuples-tttt --selection trg-dilepVL-minDLmass20-offZ1-njet4p-btag2p-ht500 --sample TTLep_bb     --config tttt_2l_bb
 # python make_ntuple.py  --output /eos/vbc/group/cms/$USER/tttt/training-ntuples-tttt --selection trg-dilepVL-minDLmass20-offZ1-njet4p-btag2p-ht500 --sample TTLep_cc     --config tttt_2l_bb
 python make_ntuple.py  --output /eos/vbc/group/cms/$USER/tttt/training-ntuples-tttt --selection trg-dilepVL-minDLmass20-offZ1-njet4p-btag2p-ht500 --sample TTLep_other  --config tttt_2l_bb
+
+#python make_ntuple.py  --output /eos/vbc/group/cms/$USER/tttt/training-ntuples-ttbb --selection trg-dilepVL-minDLmass20-offZ1-njet4p-btag2p-ht500 --sample TTLep_bb  --config ttbb_eft
+#python make_ntuple.py  --output /eos/vbc/group/cms/$USER/tttt/training-ntuples-ttbb --selection trg-dilepVL-minDLmass20-offZ1-njet4p-btag2p-ht500 --sample TTLep_bb_EFT  --config ttbb_eft
---
+++ Tools/scripts/rebin.py
@@ -0,0 +1,44 @@
+#!/env/python
+import os
+import ROOT
+import argparse
+import Analysis.Tools.helpers as helpers
+
+#Logger
+import tttt.Tools.logger as logger
+logger = logger.get_logger("INFO", logFile = None )
+
+argParser = argparse.ArgumentParser(description = "Argument parser")
+argParser.add_argument('--logLevel', action='store', nargs='?',  choices=['CRITICAL', 'ERROR', 'WARNING', 'INFO', 'DEBUG', 'TRACE', 'NOTSET'],   default='INFO', help="Log level for logging" )
+argParser.add_argument('--files',                     action='store', nargs='*')
+argParser.add_argument('--rebin',                     action='store', type=int, required=True)
+argParser.add_argument('--outdir',                    action='store', type=str,   default=None)
+
+args = argParser.parse_args()
+
+for filename in args.files:
+    if not filename.endswith('.root'):
+        logger.warning( "Not a root file: %s"%filename)
+
+    logger.info( "At file %s"%filename )
+
+    gDir = ROOT.gDirectory.GetName()
+    f = ROOT.TFile.Open( filename )
+    keys   = f.GetListOfKeys()
+    names = [keys.At(i_key).GetName() for i_key in range(keys.GetSize())]
+    f.Close()
+    ROOT.gDirectory.cd(gDir+':/')
+ 
+    if args.outdir == None:
+        outdir = os.path.dirname(filename)+'_rebin%i'%args.rebin
+    else:
+        outdir = args.outdir
+
+    outfile = os.path.join( outdir, os.path.basename( filename ))
+
+    for t in names:
+        h = helpers.getObjFromFile( filename, t)
+        h.Rebin(args.rebin)
+        helpers.writeObjToFile( outfile, h, update=True)
+
+    logger.info ("Done with file %s", outfile)
