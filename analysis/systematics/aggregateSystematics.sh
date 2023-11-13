87f626f9a5bc86d4a7b45b04b6c323acd76e8fa3
Maryam Shooshtari <mary.shooshtari@gmail.com>
2023-11-13 12:11:38 +0100

    modification to allow first EFT fit

--- analysis/systematics/aggregateSystematics.sh
+++ analysis/systematics/aggregateSystematics.sh
@@ -1,28 +1,13 @@
-python aggregateSystematics.py --plot_directory 4t-v10.2-syst --selection trg-dilep-OS-minDLmass20-offZ1-lepVeto2-njet8p-btag3p-ht500
-python aggregateSystematics.py --plot_directory 4t-v10.2-syst --selection trg-dilep-OS-minDLmass20-offZ1-lepVeto2-njet8p-btag2-ht500
-python aggregateSystematics.py --plot_directory 4t-v10.2-syst --selection trg-dilep-OS-minDLmass20-offZ1-lepVeto2-njet8p-btag1-ht500
-python aggregateSystematics.py --plot_directory 4t-v10.2-syst --selection trg-dilep-OS-minDLmass20-offZ1-lepVeto2-njet6to7-btag3p-ht500
-python aggregateSystematics.py --plot_directory 4t-v10.2-syst --selection trg-dilep-OS-minDLmass20-offZ1-lepVeto2-njet6to7-btag2-ht500
-python aggregateSystematics.py --plot_directory 4t-v10.2-syst --selection trg-dilep-OS-minDLmass20-offZ1-lepVeto2-njet6to7-btag1-ht500
-python aggregateSystematics.py --plot_directory 4t-v10.2-syst --selection trg-dilep-OS-minDLmass20-offZ1-lepVeto2-njet4to5-btag3p-ht500
-python aggregateSystematics.py --plot_directory 4t-v10.2-syst --selection trg-dilep-OS-minDLmass20-offZ1-lepVeto2-njet4to5-btag2-ht500
-python aggregateSystematics.py --plot_directory 4t-v10.2-syst --selection trg-dilep-OS-minDLmass20-offZ1-lepVeto2-njet4to5-btag1-ht500
-
-python aggregateSystematics.py --plot_directory 4t-v10.2-syst --selection trg-dilep-OS-minDLmass20-offZ1-lepVeto2-njet4p-btag1p-ht500
-python aggregateSystematics.py --plot_directory 4t-v10.2-syst --selection trg-dilep-OS-minDLmass20-offZ1-lepVeto2-njet5p-btag1p-ht500
-python aggregateSystematics.py --plot_directory 4t-v10.2-syst --selection trg-dilep-OS-minDLmass20-offZ1-lepVeto2-njet6p-btag1p-ht500
-python aggregateSystematics.py --plot_directory 4t-v10.2-syst --selection trg-dilep-OS-minDLmass20-offZ1-lepVeto2-njet7p-btag1p-ht500
-python aggregateSystematics.py --plot_directory 4t-v10.2-syst --selection trg-dilep-OS-minDLmass20-offZ1-lepVeto2-njet8p-btag1p-ht500
-
-python aggregateSystematics.py --plot_directory 4t-v10.2-syst --selection trg-dilep-OS-minDLmass20-offZ1-lepVeto2-njet4p-btag2p-ht500
-python aggregateSystematics.py --plot_directory 4t-v10.2-syst --selection trg-dilep-OS-minDLmass20-offZ1-lepVeto2-njet5p-btag2p-ht500
-python aggregateSystematics.py --plot_directory 4t-v10.2-syst --selection trg-dilep-OS-minDLmass20-offZ1-lepVeto2-njet6p-btag2p-ht500
-python aggregateSystematics.py --plot_directory 4t-v10.2-syst --selection trg-dilep-OS-minDLmass20-offZ1-lepVeto2-njet7p-btag2p-ht500
-python aggregateSystematics.py --plot_directory 4t-v10.2-syst --selection trg-dilep-OS-minDLmass20-offZ1-lepVeto2-njet8p-btag2p-ht500
-
-python aggregateSystematics.py --plot_directory 4t-v10.2-syst --selection trg-dilep-OS-minDLmass20-offZ1-lepVeto2-njet4p-btag3p-ht500
-python aggregateSystematics.py --plot_directory 4t-v10.2-syst --selection trg-dilep-OS-minDLmass20-offZ1-lepVeto2-njet5p-btag3p-ht500
-python aggregateSystematics.py --plot_directory 4t-v10.2-syst --selection trg-dilep-OS-minDLmass20-offZ1-lepVeto2-njet6p-btag3p-ht500
-python aggregateSystematics.py --plot_directory 4t-v10.2-syst --selection trg-dilep-OS-minDLmass20-offZ1-lepVeto2-njet7p-btag3p-ht500
-python aggregateSystematics.py --plot_directory 4t-v10.2-syst --selection trg-dilep-OS-minDLmass20-offZ1-lepVeto2-njet8p-btag3p-ht500
-
+python aggregateSystematics.py --plot_directory 4t-v12-syst --selection trg-dilep-OS-minDLmass20-offZ1-lepVeto2-njet8p-btag3p-ht500
+python aggregateSystematics.py --plot_directory 4t-v12-syst --selection trg-dilep-OS-minDLmass20-offZ1-lepVeto2-njet8p-btag2-ht500
+python aggregateSystematics.py --plot_directory 4t-v12-syst --selection trg-dilep-OS-minDLmass20-offZ1-lepVeto2-njet8p-btag1-ht500
+python aggregateSystematics.py --plot_directory 4t-v12-syst --selection trg-dilep-OS-minDLmass20-offZ1-lepVeto2-njet6to7-btag3p-ht500
+python aggregateSystematics.py --plot_directory 4t-v12-syst_mvaCut0.1m --selection trg-dilep-OS-minDLmass20-offZ1-lepVeto2-njet6to7-btag3p-ht500 --out_directory 4t-v12-syst
+python aggregateSystematics.py --plot_directory 4t-v12-syst_mvaCut0.1p --selection trg-dilep-OS-minDLmass20-offZ1-lepVeto2-njet6to7-btag3p-ht500 --out_directory 4t-v12-syst
+python aggregateSystematics.py --plot_directory 4t-v12-syst --selection trg-dilep-OS-minDLmass20-offZ1-lepVeto2-njet6to7-btag2-ht500
+python aggregateSystematics.py --plot_directory 4t-v12-syst --selection trg-dilep-OS-minDLmass20-offZ1-lepVeto2-njet6to7-btag1-ht500
+python aggregateSystematics.py --plot_directory 4t-v12-syst --selection trg-dilep-OS-minDLmass20-offZ1-lepVeto2-njet4to5-btag3p-ht500
+python aggregateSystematics.py --plot_directory 4t-v12-syst_mvaCut0.2m --selection trg-dilep-OS-minDLmass20-offZ1-lepVeto2-njet4to5-btag3p-ht500 --out_directory 4t-v12-syst
+python aggregateSystematics.py --plot_directory 4t-v12-syst_mvaCut0.2p --selection trg-dilep-OS-minDLmass20-offZ1-lepVeto2-njet4to5-btag3p-ht500 --out_directory 4t-v12-syst
+python aggregateSystematics.py --plot_directory 4t-v12-syst --selection trg-dilep-OS-minDLmass20-offZ1-lepVeto2-njet4to5-btag2-ht500
+python aggregateSystematics.py --plot_directory 4t-v12-syst --selection trg-dilep-OS-minDLmass20-offZ1-lepVeto2-njet4to5-btag1-ht500
--- analysis/systematics/dataCards/combineTool.sh
+++ analysis/systematics/dataCards/combineTool.sh
@@ -8,11 +8,18 @@
 
 #How to run Combine commands without going crazy :)
 
-declare -A regions=(['njet4to5_btag3p']='njet4to5-btag3p' ['njet4to5_btag2']='njet4to5-btag2' ['njet4to5_btag1']='njet4to5-btag1'
-                    ['njet6to7_btag2']='njet6to7-btag2' ['njet6to7_btag1']='njet6to7-btag1'
-                    ['njet8p_btag2']='njet8p-btag2' ['njet8p_btag1']='njet8p-btag1')
+declare -A regions=(
+		    ['mvaCut0.2p_njet4to5_btag3p']='mvaCut0.2p_njet4to5_btag3p' 
+		    ['mvaCut0.2m_njet4to5_btag3p']='mvaCut0.2m_njet4to5_btag3p' 
+		    ['njet4to5_btag2']='njet4to5_btag2' ['njet4to5_btag1']='njet4to5_btag1'
+                    ['njet6to7_btag2']='njet6to7_btag2' ['njet6to7_btag1']='njet6to7_btag1'
+                    ['njet8p_btag2']='njet8p_btag2' ['njet8p_btag1']='njet8p_btag1'
+		    ['njet8p_btag3p']='njet8p_btag3p' 
+		    ['mvaCut0.1p_njet6to7_btag3p']='mvaCut0.1p_njet6to7_btag3p'
+		    ['mvaCut0.1m_njet6to7_btag3p']='mvaCut0.1m_njet6to7_btag3p'
+		    )
 declare -A variables=(['mva']='2l_4t' ['nJetGood']='nJetGood' ['nBTag']='nBTag' ['ht']='ht')
-declare -A masking=(['nJetGood']='nJetGood' ['nBTag']='nBTag' ['ht']='ht')
+declare -A masking=(['nJetGood']='nJetGood' ['nBTag']='nBTag' ['mva']='2l_4t')
 
 unite=false
 impact=false
@@ -46,23 +53,23 @@ if $unite;then
   theEndlessScroll="combineCards.py "
   for variable in "${!variables[@]}";do
     for region in "${!regions[@]}";do
-      theEndlessScroll+="$variable""_$region=tttt_${variables[$variable]}""_1_13TeV_${regions[$region]}"".txt "
+      theEndlessScroll+="$variable""_$region=ttbbEFT_${variables[$variable]}""_1_13TeV_${regions[$region]}"".txt "
     done
   done
-  theEndlessScroll+=">& CRs_combined.txt"
+  theEndlessScroll+=">& combined.txt"
   eval "$theEndlessScroll"
   wait
-  submit "text2workspace.py CRs_combined.txt --channel-masks"
+  echo "text2workspace.py combined.txt --channel-masks"
   wait
 elif $impact; then
   echo "Executing (Second) Impact"
-  python combineTool.py -M Impacts -d CRs_combined.root -m 125 -t -1 --doInitialFit --robustFit 1 --expectSignal=1  --freezeNuisanceGroups=theory
-  python combineTool.py -M Impacts -d CRs_combined.root -m 125 -t -1 --doFits --robustFit 1 --expectSignal=1   --freezeNuisanceGroups=theory --parallel 10
-  python combineTool.py -M Impacts -d CRs_combined.root -m 125 -o impacts_CRs_combined.json
-  python ../plotImpacts.py -i impacts_CRs_combined.json -o impacts_CRs_combined
+  python combineTool.py -M Impacts -d combined.root -m 125 -t -1 --doInitialFit --robustFit 1 --expectSignal=1  --freezeNuisanceGroups=theory
+  python combineTool.py -M Impacts -d combined.root -m 125 -t -1 --doFits --robustFit 1 --expectSignal=1   --freezeNuisanceGroups=theory --parallel 10
+  python combineTool.py -M Impacts -d combined.root -m 125 -o impacts_combined.json
+  python ../plotImpacts.py -i impacts_combined.json -o impacts_combined
 elif $postfit; then
   echo "Executing Postfit."
-  neverendingStory="combine CRs_combined.root -M FitDiagnostics --saveShapes --saveWithUnc  -1 -n .postFit_combined --setParameterRange r=-19,20 --expectSignal=1 --plots --freezeNuisanceGroups=theory --ignoreCovWarning --setParameters "
+  neverendingStory="combine combined.root -M FitDiagnostics --saveShapes --saveWithUnc  -n .postFit_combined --setParameterRange r=-19,20 --expectSignal=0 --plots  --ignoreCovWarning --setParameters "
   for mask in "${!masking[@]}";do
     for region in "${!regions[@]}";do
       neverendingStory+="mask_$mask""_$region""=1,"
@@ -70,13 +77,13 @@ elif $postfit; then
   done
   length=${#neverendingStory}
   lastHurra="${neverendingStory:0:length-1}"
-  lastHurra+=" -t -1"
-  $lastHurra
-  python postFitPlotter.py --inputFile dataCards/fitDiagnostics.postFit_combined.root --backgroundOnly
+  #lastHurra+=" -t -1"
+  echo $lastHurra
+  #python postFitPlotter.py --inputFile dataCards/fitDiagnostics.postFit_combined.root --backgroundOnly
 
-elif $impact; then
+elif $multi; then
   echo "Executing Multi."
-  neverendingStory="combine CRs_combined.root -M MultiDimFit --saveWorkspace  -t -1 --algo grid --points 100 --setParameterRange r=-19,20 -n .combinedFit --expectSignal=1 --freezeNuisanceGroups=theory"
+  neverendingStory="combine combined.root -M MultiDimFit --saveWorkspace  -t -1 --algo grid --points 100 --setParameterRange r=-5,5 -n .combinedFit --expectSignal=1 --setParameters "
   for mask in "${!masking[@]}";do
     for region in "${!regions[@]}";do
       neverendingStory+="mask_$mask""_$region""=1,"
@@ -84,9 +91,9 @@ elif $impact; then
   done
   length=${#neverendingStory}
   lastHurra="${neverendingStory:0:length-1}"
-  lastHurra+=" -t -1"
-  $lastHurra
-  python ../plot1Dscan.py higgsCombine.combinedFit.MultiDimFit.mH120.root  --selection CRs --output -o CRs_combined
+  #lastHurra+=" -t -1"
+  echo $lastHurra
+  #python ../plot1Dscan.py higgsCombine.combinedFit.MultiDimFit.mH120.root  --selection CRs --output -o CRs_combined
 else
   echo "Any other business"
 fi
