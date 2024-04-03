4fa5055d366af60b1dcb03a0ae1d9b12b1cf6dcb
Maryam Shooshtari <mary.shooshtari@gmail.com>
2024-04-03 12:21:40 +0200

    updated script to run combine

--- analysis/systematics/dataCards/combineTool.sh
+++ analysis/systematics/dataCards/combineTool.sh
@@ -8,28 +8,40 @@
 
 #How to run Combine commands without going crazy :)
 declare -A regions=(
-		   ['njet4to5_btag3p']='njet4to5_btag3p' ['njet4to5_btag2']='njet4to5_btag2' ['njet4to5_btag1']='njet4to5_btag1'
-           ['njet6p_btag3p']='njet6p_btag3p'     ['njet6p_btag2']='njet6p_btag2'     ['njet6p_btag1']='njet6p_btag1'
-#            ['njet6to7_btag2']='njet6to7_btag2' ['njet6to7_btag1']='njet6to7_btag1'
-#            ['njet8p_btag2']='njet8p_btag2' ['njet8p_btag1']='njet8p_btag1'
-#		    ['njet8p_btag3p']='njet8p_btag3p' 
-#		    ['mvaCut1p_njet6to7_btag3p']='mvaCut0.1p_njet6to7_btag3p'
-#		    ['mvaCut1m_njet6to7_btag3p']='mvaCut0.1m_njet6to7_btag3p'
-#		    ['mvaCut2p_njet4to5_btag3p']='mvaCut0.2p_njet4to5_btag3p' 
-#		    ['mvaCut2m_njet4to5_btag3p']='mvaCut0.2m_njet4to5_btag3p' 
+		    ['njet4to5_btag3p']='njet4to5_btag3p'   ['njet4to5_btag2']='njet4to5_btag2' ['njet4to5_btag1']='njet4to5_btag1'
+            ['njet6to7_btag3p']='njet6to7_btag3p'   ['njet6to7_btag2']='njet6to7_btag2' ['njet6to7_btag1']='njet6to7_btag1'
+            ['njet8p_btag3p']='njet8p_btag3p'       ['njet8p_btag2']='njet8p_btag2'     ['njet8p_btag1']='njet8p_btag1' 
+#           ['njet6p_btag3p']='njet6p_btag3p'     ['njet6p_btag2']='njet6p_btag2'     ['njet6p_btag1']='njet6p_btag1'
 		    )
 
-declare -A variables=(['ht']='ht') #(['mva']='2l_4t' ['nJetGood']='nJetGood' ['nBTag']='nBTag' ['ht']='ht')
-declare -A masking=(['nJetGood']='nJetGood' ['nBTag']='nBTag' ['mva']='2l_4t')
+declare -A variables=(['ht']='ht' ['mva']='2l_4t' ['nJetGood']='nJetGood' ['nBTag']='nBTag')
+declare -A masking=(['nJetGood']='nJetGood' ['nBTag']='nBTag') # ['mva']='2l_4t')
+
+declare -A ht_mask_regions=(
+		    ['njet4to5_btag3p']='njet4to5_btag3p'
+            ['njet6to7_btag3p']='njet6to7_btag3p'
+            ['njet8p_btag3p']='njet8p_btag3p'    
+            )
+declare -A mva_mask_regions=(
+		       ['njet4to5_btag2']='njet4to5_btag2' ['njet4to5_btag1']='njet4to5_btag1'
+               ['njet6to7_btag2']='njet6to7_btag2' ['njet6to7_btag1']='njet6to7_btag1'
+               ['njet8p_btag2']='njet8p_btag2'     ['njet8p_btag1']='njet8p_btag1' 
+                )
+for region in "${!ht_mask_regions[@]}";do
+    neverendingStory+="mask_ht_$region""=1,"
+done
+for region in "${!mva_mask_regions[@]}";do
+    neverendingStory+="mask_mva_$region""=1,"
+done
 
 for mask in "${!masking[@]}";do
   for region in "${!regions[@]}";do
-    neverendingStory+=" mask_$mask""_$region""=1,"
+    neverendingStory+="mask_$mask""_$region""=1,"
   done
 done
 length=${#neverendingStory}
 maskingString="${neverendingStory:0:length-1}"
-# echo $maskingString
+#echo $neverendingStory
 
 unite=false
 impact=false
@@ -64,49 +76,38 @@ if $unite;then
   theEndlessScroll="combineCards.py "
   for variable in "${!variables[@]}";do
     for region in "${!regions[@]}";do
-      theEndlessScroll+="$variable""_$region=ttbbEFT_${variables[$variable]}""_1_13TeV_${regions[$region]}"".txt "
+      theEndlessScroll+="$variable""_$region=ttttEFT_${variables[$variable]}""_1_13TeV_${regions[$region]}"".txt "
     done
   done
-  echo $theEndlessScroll
   theEndlessScroll+=">& combined.txt"
   eval "$theEndlessScroll"
   echo "* autoMCStats 0 1" >> combined.txt
   wait
-  echo "text2workspace.py combined.txt --channel-masks"
+  eval "text2workspace.py combined.txt --channel-masks -P HiggsAnalysis.AnalyticAnomalousCoupling.AnomalousCouplingEFTNegative:analiticAnomalousCouplingEFTNegative --X-allow-no-signal --PO eftOperators=cQQ1"
   wait
+
 elif $impact; then
+#not updated
   echo "Executing (Second) Impact"
   python combineTool.py -M Impacts -d combined.root -m 125 -t -1 --doInitialFit --robustFit 1 --expectSignal=1  --freezeNuisanceGroups=theory
   python combineTool.py -M Impacts -d combined.root -m 125 -t -1 --doFits --robustFit 1 --expectSignal=1   --freezeNuisanceGroups=theory --parallel 10
   python combineTool.py -M Impacts -d combined.root -m 125 -o impacts_combined.json
   python ../plotImpacts.py -i impacts_combined.json -o impacts_combined
+
 elif $postfit; then
   echo "Executing Postfit."
-  neverendingStory="combine combined.root -M FitDiagnostics --saveShapes --saveWithUnc  -n .postFit_combined --setParameterRange r=-19,20 --expectSignal=0 --plots  --ignoreCovWarning --setParameters "
-  for mask in "${!masking[@]}";do
-    for region in "${!regions[@]}";do
-      neverendingStory+="mask_$mask""_$region""=1,"
-    done
-  done
-  length=${#neverendingStory}
-  lastHurra="${neverendingStory:0:length-1}"
-  #lastHurra+=" -t -1"
-  echo $lastHurra
+  diagnosticStory="combine combined.root -t -1 -M FitDiagnostics --plots --saveShapes --saveWithUnc -n .postFit_combined  --cminDefaultMinimizerStrategy=0 --redefineSignalPOIs k_cQQ1 --freezeParameters r --setParameterRanges k_cQQ1=-10,10  --setParameters r=1,"
+  diagnosticStory+="${neverendingStory}"
+  echo $diagnosticStory
   #python postFitPlotter.py --inputFile dataCards/fitDiagnostics.postFit_combined.root --backgroundOnly
 
 elif $multi; then
   echo "Executing Multi."
-  neverendingStory="combine combined.root -M MultiDimFit --saveWorkspace  -t -1 --algo grid --points 100 --setParameterRange r=-5,5 -n .combinedFit --expectSignal=1 --setParameters "
-  for mask in "${!masking[@]}";do
-    for region in "${!regions[@]}";do
-      neverendingStory+="mask_$mask""_$region""=1,"
-    done
-  done
-  length=${#neverendingStory}
-  lastHurra="${neverendingStory:0:length-1}"
-  #lastHurra+=" -t -1"
-  echo $lastHurra
+  multidimStory="combine combined.root -t -1 -M MultiDimFit --saveWorkspace --algo grid --points 100 -n .combinedFit --redefineSignalPOIs k_cQQ1 --freezeParameters r --setParameterRanges k_cQQ1=-10,10 --setParameters r=1,"
+  multidimStory+="${neverendingStory}"
+  echo $multidimStory
   #python ../plot1Dscan.py higgsCombine.combinedFit.MultiDimFit.mH120.root  --selection CRs --output -o CRs_combined
+
 else
-  echo "Any other business"
+  echo "I did the thing"
 fi
