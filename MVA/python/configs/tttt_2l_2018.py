7063e62d11075ae4a3ceb78d31b3321d1a0bc8f1
cristina.giordano <cristina.giordano@oeaw.ac.at>
2024-05-31 10:38:12 +0200

    tmp config split for systematics

--- MVA/python/configs/tttt_2l_2018.py
+++ MVA/python/configs/tttt_2l_2018.py
@@ -279,34 +279,34 @@ def predict_inputs( event, sample, jet_lstm = True):
         return  flat_variables
 
 #define training samples for multiclassification
-# import tttt.samples.nano_mc_private_UL20_Fall17_postProcessed_dilep as samples
-# # import tttt.samples.nano_private_UL20_RunII_postProcessed_dilep as samples
-#
-# # ttbar gen classification: https://github.com/cms-top/cmssw/blob/topNanoV6_from-CMSSW_10_2_18/TopQuarkAnalysis/TopTools/plugins/GenTtbarCategorizer.cc
-# sample_TTLep = samples.TTLepbb
-# # ttbar gen classification: https://github.com/cms-top/cmssw/blob/topNanoV6_from-CMSSW_10_2_18/TopQuarkAnalysis/TopTools/plugins/GenTtbarCategorizer.cc
-#
-# TTLep_bb    = copy.deepcopy( sample_TTLep )
-# TTLep_bb.name = "TTLep_bb"
-# TTLep_bb.texName = "t#bar{t}b#bar{b}"
-# TTLep_bb.color   = ROOT.kRed + 2
-# TTLep_bb.setSelectionString( "genTtbarId%100>=50&&overlapRemoval" )
-# TTLep_cc    = copy.deepcopy( sample_TTLep )
-# TTLep_cc.name = "TTLep_cc"
-# TTLep_cc.texName = "t#bar{t}c#bar{c}"
-# TTLep_cc.color   = ROOT.kRed - 3
-# TTLep_cc.setSelectionString( "genTtbarId%100>=40&&genTtbarId%100<50&&overlapRemoval" )
-# TTLep_other = copy.deepcopy( sample_TTLep )
-# TTLep_other.name = "TTLep_other"
-# TTLep_other.texName = "t#bar{t} + light j."
-# TTLep_other.setSelectionString( "genTtbarId%100<40&&overlapRemoval" )
-#
-# training_samples = [ samples.TTTT, TTLep_bb, TTLep_cc, TTLep_other]#, samples.ST, samples.TTW, samples.TTH, samples.TTZ]
-# # training_samples = [samples.TTTT, sample_TTLep]
-#
-# # classes          = [ "2l_4t", "2l_tt"]#,"2l_ttcc","2l_ttlight"]
-# classes          = [ "2l_4t", "2l_tt","2l_ttcc","2l_ttlight"]
-# assert len(training_samples)==len(set([s.name for s in training_samples])), "training_samples names are not unique!"
+
+import tttt.samples.nano_mc_private_UL20_Autumn18_postProcessed_dilep as samples
+
+# ttbar gen classification: https://github.com/cms-top/cmssw/blob/topNanoV6_from-CMSSW_10_2_18/TopQuarkAnalysis/TopTools/plugins/GenTtbarCategorizer.cc
+sample_TTLep = samples.TTLepbb
+# ttbar gen classification: https://github.com/cms-top/cmssw/blob/topNanoV6_from-CMSSW_10_2_18/TopQuarkAnalysis/TopTools/plugins/GenTtbarCategorizer.cc
+
+TTLep_bb    = copy.deepcopy( sample_TTLep )
+TTLep_bb.name = "TTLep_bb"
+TTLep_bb.texName = "t#bar{t}b#bar{b}"
+TTLep_bb.color   = ROOT.kRed + 2
+TTLep_bb.setSelectionString( "genTtbarId%100>=50&&overlapRemoval" )
+TTLep_cc    = copy.deepcopy( sample_TTLep )
+TTLep_cc.name = "TTLep_cc"
+TTLep_cc.texName = "t#bar{t}c#bar{c}"
+TTLep_cc.color   = ROOT.kRed - 3
+TTLep_cc.setSelectionString( "genTtbarId%100>=40&&genTtbarId%100<50&&overlapRemoval" )
+TTLep_other = copy.deepcopy( sample_TTLep )
+TTLep_other.name = "TTLep_other"
+TTLep_other.texName = "t#bar{t} + light j."
+TTLep_other.setSelectionString( "genTtbarId%100<40&&overlapRemoval" )
+
+training_samples = [ samples.TTTT, TTLep_bb, TTLep_cc, TTLep_other]#, samples.ST, samples.TTW, samples.TTH, samples.TTZ]
+# training_samples = [samples.TTTT, sample_TTLep]
+
+# classes          = [ "2l_4t", "2l_tt"]#,"2l_ttcc","2l_ttlight"]
+classes          = [ "2l_4t", "2l_tt","2l_ttcc","2l_ttlight"]
+assert len(training_samples)==len(set([s.name for s in training_samples])), "training_samples names are not unique!"
 
 # training selection
 
